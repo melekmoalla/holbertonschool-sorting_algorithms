cf5abcc660d3667c980d5ae921bb0e3bb64ff830
melekmoalla <melekmoalla2468@gmail.com>
2022-12-20 19:30:30 +0100

    test

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * print_array - Prints an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+void bubble_sort(int *array, size_t size)
+{
+    int temp;
+    size_t i = 0, j = 0;
+
+    for (i = 0; i < size - 1; i++)
+    {
+        for (j = 0; j < size - i; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+                print_array(array, size);
+            }
+        }
+    }
+}
