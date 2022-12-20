fba1f6f26642a44586524cd39d3e27eb40cd4b6c
melekmoalla <melekmoalla2468@gmail.com>
2022-12-20 19:31:17 +0100

    add

---
+++ sort.h
@@ -0,0 +1,36 @@
+#ifndef INV_TREE_H
+#define INV_TREE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <string.h>
+#include <signal.h>
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+
+void selection_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void bubble_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void quick_sort(int *array, size_t size);
+
+#endif
