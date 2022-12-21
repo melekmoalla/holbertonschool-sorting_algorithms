0fd674182ef593437e15e0b77607edd36dbda623
melekmoalla <melekmoalla2468@gmail.com>
2022-12-21 01:24:11 +0100

    test

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,35 +2,40 @@
 #include <stdio.h>
 #include "sort.h"
 
+/**
+ * insertion_sort_list - order using the Insertion sort algorithm
+ * @list: listint_t
+ */
+
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *temp;
+	listint_t *temp;
 
-    temp = (*list)->next;
-    if (list == NULL || *list == NULL)
-        return;
-    while (temp != NULL)
-    {
-        while (temp->prev != NULL && temp->prev->n > temp->n)
-        {
-            temp->prev->next = temp->next;
-            if (temp->next != NULL)
-            {
-                temp->next->prev = temp->prev;
-            }
-            temp->next = temp->prev;
-            temp->prev = temp->prev->prev;
-            temp->next->prev = temp;
-            if (temp->prev == NULL)
-            {
-                (*list) = temp;
-            }
-            else
-            {
-                temp->prev->next = temp;
-            }
-            print_list(*list);
-        }
-        temp = temp->next;
-    }
+	temp = (*list)->next;
+	if (list == NULL || *list == NULL)
+		return;
+	while (temp != NULL)
+	{
+		while (temp->prev != NULL && temp->prev->n > temp->n)
+		{
+			temp->prev->next = temp->next;
+			if (temp->next != NULL)
+			{
+				temp->next->prev = temp->prev;
+			}
+			temp->next = temp->prev;
+			temp->prev = temp->prev->prev;
+			temp->next->prev = temp;
+			if (temp->prev == NULL)
+			{
+				(*list) = temp;
+			}
+			else
+			{
+				temp->prev->next = temp;
+			}
+			print_list(*list);
+		}
+		temp = temp->next;
+	}
 }
