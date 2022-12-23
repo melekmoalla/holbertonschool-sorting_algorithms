adfde0c98f4447f2c33882f6dcb21bf8520a47a3
melekmoalla <melekmoalla2468@gmail.com>
2022-12-23 19:57:01 +0100

    test go

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,35 +1,80 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "sort.h"
+#include <assert.h>
+size_t a;
+int temp;
 
-void quickk_sort(int *arr, size_t size)
+/**
+ * swap - fonction that do swap betwen to integer
+ * @arr: The array to be printed
+ * @i: int
+ * @j: int
+ */
+
+void swap(int *arr, int i, int j)
 {
+	temp = arr[i];
+	arr[i] = arr[j];
+	arr[j] = temp;
+}
 
-	size_t i, j;
-	int temp;
-	for (i = 0; i < size - 2; i++)
+/**
+ * partition - fonction that do the condition swap
+ * @arr: The array to be printed
+ * @start: Number of elements in @array
+ * @end: int
+ * Return: i
+ */
+
+int partition(int *arr, int start, int end)
+{
+	int pivot = arr[end];
+	int i = start;
+	int j;
+
+	for (j = start; j < end; j++)
 	{
-		int min_index = i;
-		for (j = i + 1; j < size; j++)
+		if (arr[j] < pivot)
 		{
-			if (arr[j] < arr[min_index])
-			{
-				min_index = j;
-			}
-		}
-
-		temp = arr[i];
-		arr[i] = arr[min_index];
-		arr[min_index] = temp;
-		if (arr[i] != temp)
-		{
-			print_array(arr, size);
+			swap(arr, i, j);
+			i++;
 		}
 	}
+	swap(arr, i, end);
+	if (arr[i] != temp)
+		print_array(arr, a);
+	return (i);
 }
+
+/**
+ * sort - fonction that do
+ * @arr: The array to be printed
+ * @start: the start of the array
+ * @end: the end  of the array
+ */
+
+void sort(int *arr, int start, int end)
+{
+
+	if (start < end)
+	{
+		int pivot_index = partition(arr, start, end);
+
+		sort(arr, start, pivot_index - 1);
+		sort(arr, pivot_index + 1, end);
+	}
+}
+
+/**
+ * quick_sort - function that sorts an array of integers
+ * @arr: The array to be printed
+ * @size: Number of elements in @array
+ */
+
 void quick_sort(int *arr, size_t size)
 {
-	if (arr == NULL || size < 2)
-		return;
-	quickk_sort(arr, size);
+	a = size;
+
+	sort(arr, 0, size - 1);
 }
