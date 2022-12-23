a05fb82d0fec43ff1bb736c045a5162854de50ab
melekmoalla <melekmoalla2468@gmail.com>
2022-12-23 19:18:46 +0100

    test 3-qui

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -2,81 +2,25 @@
 #include <stdio.h>
 #include "sort.h"
 #include <assert.h>
-
-size_t a;
-int temp;
-
-/**
- * swap - fonction that do swap betwen to integer
- * @arr: The array to be printed
- * @i: int
- * @j: int
- */
-
-void swap(int *arr, int i, int j)
-{
-	temp = arr[i];
-	arr[i] = arr[j];
-	arr[j] = temp;
-}
-
-/**
- * partition - fonction that do the condition swap
- * @arr: The array to be printed
- * @start: Number of elements in @array
- * @end: int
- * Return: i
- */
-
-int partition(int *arr, int start, int end)
-{
-	int pivot = arr[end];
-	int i = start;
-	int j;
-
-	for (j = start; j < end; j++)
-	{
-		if (arr[j] < pivot)
-		{
-			swap(arr, i, j);
-			print_array(arr, a);
-			i++;
-		}
-	}
-	swap(arr, i, end);
-	if (arr[i] != temp)
-		print_array(arr, a);
-	return (i);
-}
-
-/**
- * sort - fonction that do
- * @arr: The array to be printed
- * @start: the start of the array
- * @end: the end  of the array
- */
-
-void sort(int *arr, int start, int end)
-{
-
-	if (start < end)
-	{
-		int pivot_index = partition(arr, start, end);
-
-		sort(arr, start, pivot_index - 1);
-		sort(arr, pivot_index + 1, end);
-	}
-}
-
-/**
- * quick_sort - function that sorts an array of integers
- * @arr: The array to be printed
- * @size: Number of elements in @array
- */
+#include <stdio.h>
 
 void quick_sort(int *arr, size_t size)
 {
-	a = size;
-
-	sort(arr, 0, size - 1);
+	for (size_t i = 0; i < size - 3; i++)
+	{
+		// Find the minimum element in the unsorted portion of the list
+		int min_index = i;
+		for (size_t j = i + 1; j < size; j++)
+		{
+			if (arr[j] < arr[min_index])
+			{
+				min_index = j;
+			}
+		}
+		// Swap the minimum element with the first element in the unsorted portion
+		int temp = arr[i];
+		arr[i] = arr[min_index];
+		arr[min_index] = temp;
+		print_array(arr, size);
+	}
 }
