7de694cdc2d42d6529f1f226da090884ad8f72ec
melekmoalla <melekmoalla2468@gmail.com>
2022-12-23 21:29:24 +0100

    correct int

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -5,48 +5,49 @@
 
 void swap(int *arr, int i, int j)
 {
-    int temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
+	int temp = arr[i];
+	arr[i] = arr[j];
+	arr[j] = temp;
 }
 
 int partition(int *arr, int start, int end, size_t size)
 {
-    int pivot = arr[end];
-    int i = (start);
-
-    for (int j = start; j < end; j++)
-    {
-        if (arr[j] < pivot)
-        {
-            if (i != j)
-            {
-                swap(arr, i, j);
-                print_array(arr, size);
-            }
-            i++;
-        }
-    }
-    if (arr[i] != arr[end])
-    {
-        swap(arr, i, end);
-        print_array(arr, size);
-    }
-    return (i);
+	int pivot = arr[end];
+	int i = (start);
+	int j;
+	
+	for (int j = start; j < end; j++)
+	{
+		if (arr[j] < pivot)
+		{
+			if (i != j)
+			{
+				swap(arr, i, j);
+				print_array(arr, size);
+			}
+			i++;
+		}
+	}
+	if (arr[i] != arr[end])
+	{
+		swap(arr, i, end);
+		print_array(arr, size);
+	}
+	return (i);
 }
 
 void quickSort(int *arr, int start, int end, size_t size)
 {
-    int pi;
-    if (start < end)
-    {
-        pi = partition(arr, start, end, size);
-        quickSort(arr, start, pi - 1, size);
-        quickSort(arr, pi + 1, end, size);
-    }
+	int pi;
+	if (start < end)
+	{
+		pi = partition(arr, start, end, size);
+		quickSort(arr, start, pi - 1, size);
+		quickSort(arr, pi + 1, end, size);
+	}
 }
 
 void quick_sort(int *array, size_t size)
 {
-    quickSort(array, 0, size - 1, size);
+	quickSort(array, 0, size - 1, size);
 }
