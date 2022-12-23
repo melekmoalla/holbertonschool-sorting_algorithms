d1c594c497664f874cfad76cda77d61aaf740416
melekmoalla <melekmoalla2468@gmail.com>
2022-12-23 21:25:50 +0100

    test fonction

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -3,78 +3,50 @@
 #include "sort.h"
 #include <assert.h>
 
-/**
- * swap - fonction that do swap betwen to integer
- * @arr: The array to be printed
- * @i: int
- * @j: int
- */
-
 void swap(int *arr, int i, int j)
 {
-	temp = arr[i];
-	arr[i] = arr[j];
-	arr[j] = temp;
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
 }
 
-/**
- * partition - fonction that do the condition swap
- * @arr: The array to be printed
- * @start: Number of elements in @array
- * @end: int
- * Return: i
- */
-
-int partition(int *arr, int start, int end)
+int partition(int *arr, int start, int end, size_t size)
 {
-	int pivot = arr[end];
-	int i = start;
-	int j;
+    int pivot = arr[end];
+    int i = (start);
 
-	for (j = start; j < end; j++)
-	{
-		if (arr[j] < pivot)
-		{
-			swap(arr, i, j);
-
-			i++;
-		}
-	}
-	swap(arr, i, end);
-	if (arr[i] != temp)
-		print_array(arr, a);
-	return (i);
+    for (int j = start; j < end; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            if (i != j)
+            {
+                swap(arr, i, j);
+                print_array(arr, size);
+            }
+            i++;
+        }
+    }
+    if (arr[i] != arr[end])
+    {
+        swap(arr, i, end);
+        print_array(arr, size);
+    }
+    return (i);
 }
 
-/**
- * sort - fonction that do
- * @arr: The array to be printed
- * @start: the start of the array
- * @end: the end  of the array
- */
-
-void sort(int *arr, int start, int end)
+void quickSort(int *arr, int start, int end, size_t size)
 {
-
-	if (start < end)
-	{
-		int pivot_index = partition(arr, start, end);
-
-		sort(arr, start, pivot_index - 1);
-		sort(arr, pivot_index + 1, end);
-	}
+    int pi;
+    if (start < end)
+    {
+        pi = partition(arr, start, end, size);
+        quickSort(arr, start, pi - 1, size);
+        quickSort(arr, pi + 1, end, size);
+    }
 }
 
-/**
- * quick_sort - function that sorts an array of integers
- * @arr: The array to be printed
- * @size: Number of elements in @array
- */
-
-void quick_sort(int *arr, size_t size)
+void quick_sort(int *array, size_t size)
 {
-	a = size;
-	if (arr == NULL)
-		return;
-	sort(arr, 0, size - 1);
+    quickSort(array, 0, size - 1, size);
 }
